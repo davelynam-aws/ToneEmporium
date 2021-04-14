6c77db4161c58ce550acdb55c9cbca36a4e9ae74
David Lynam <davelynam@hotmail.co.uk>
2021-04-14 23:04:57 +0100

    Working on inc_category_menu category icons. Need to get the row to fit full height of div

--- detail.php
+++ detail.php
@@ -6,11 +6,13 @@ include('inc/dbconnect.inc.php' );
 // include the header file
 include('inc/header.php');
 // include the Breadcrumbs file
-include('inc/dynamicBreadcrumbs.php');
+//include('inc/dynamicBreadcrumbs.php');
 // include the Login Form 
 include('inc/inc_loginform.php');
 // include Cart Display
 
+
+include('inc/inc_category_menu.php');
 ?>
 
     <script type="text/javascript">
@@ -83,17 +85,28 @@ echo mysqli_error( $dbconnect );
 
     <!-- Page Content -->
         <div class="container">
+		<!--
            <div class="cartdisplaydiv">
-            </div>
+            </div>-->
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card mt-4">
-                        <img class="card-img-top img-fluid" src="<?php echo $row['p_image_thumb'] ?>" alt="">
+					<!--
+                        <img class="card-img-top img-fluid" src="<?php echo $row['p_image_one'] ?>" alt="">
                         <div class="card-body">
+						-->
+						<h6>Home > Double Cutaways > Gibson Custom Shop 67 SG</h6>
                             <h3 class="card-title">
-                                <?php echo $row['p_name']; ?>
-                                <?php echo $row['p_colour']; ?>
+                                <?php echo $row['p_name']; ?>                        
                             </h3>
+							<h3>
+							       <?php echo $row['p_colour']; ?>
+							</h3>
+
+							<div id="primaryImage" class="col-md-4 retro-border">
+								<a href="/detail.php?id=<?php echo $row['product_id'] ?>"><img class="card-img-top" style="width:100%; height:100%;" src="<?php echo $row['p_image_one'] ?>" alt=""></a>
+							</div>
+
                             <h4>£
                                 <?php echo $row['p_sale_price']; ?>
                             </h4>
@@ -103,7 +116,12 @@ echo mysqli_error( $dbconnect );
 
                                                   
                         
-                            <?php
+                           
+
+
+                    </div>
+
+					 <?php
 			// Loop through each row from results
 			while ( $specrow = mysqli_fetch_array( $specresult ) ) {
 				?>
@@ -157,22 +175,6 @@ echo mysqli_error( $dbconnect );
                                 </span>
               	<?php }; ?>
 
-
- 
-                         
-
-                            <a href="#" id="addtocartlink" class="btn btn-success" alt="<?php echo $row['product_id'] ?>">add to cart</a>
-                        
-                        <?php 
-                            if($_SESSION['u_level']=='admin'){
-                        ?>
-                        <div class="btn btn-danger" onclick="confirmChoice(<?php echo $row['product_id'] ?>)">Delete</div> 
-                         
-                            <a class="btn btn-warning" href="update.php?action&id=<?php echo $row['product_id'] ?>">Update</a>
-                        <?php
-                            }
-                        ?>
-                    </div>
                     </div>
                     <!-- /.card -->
 
@@ -182,62 +184,7 @@ echo mysqli_error( $dbconnect );
             </div>
             
            
-            <?php
-        // Display All Products
-$general_result = mysqli_query( $dbconnect, "SELECT * FROM `product` ORDER BY RAND() LIMIT 3" );
-  ?>
-            
-<!-- Display the product detail in the container -->
-	<div class="container mt-3">
-			<div class="search-container">
-				<div class="home-title-container"> 
-				<div class="home-featured-title"> </div>
-					<div style="clear:both"></div>
-				</div>
-    
-				<div class="row">
-<?php
-			// Loop through each row from results
-			while ( $row = mysqli_fetch_array( $general_result ) ) {
-				?>
-						<div class="col-lg-4 col-md-6 mb-4">
-							<div class="card h-100 border-primary mb-3">
-                                <h5 class="card-header card text-white bg-primary mb-3">Featured</h5>
-								<div class="image-container">
-									<a href="/detail.php?id=<?php echo $row['product_id'] ?>"><img class="card-img-top" src="<?php echo $row['p_image'] ?>" alt=""></a>
-								</div>
-								<div class="card-body">
-									<h4 class="card-title">
-										<a href="/detail.php?id=<?php echo $row['product_id'] ?>">
-											<?php echo $row['p_name']; ?>
-										</a>
-									</h4>
-<h5>£
-										<?php echo $row['p_price']; ?>
-									</h5>
-									<h5>
-										<?php echo $row['p_category']; ?>
-									</h5>
-									<p class="card-text">
-										<?php echo $row['p_detail-thumb'] ?>
-									</p>
-								
-								</div>
-								<div class="card-footer bg-transparent border-primary">
-									<div class="btn btn-success quickAdd" data="<?php echo $row['product_id'] ?>">Add <i class="fa fa-shopping-basket"></i></div>
-									<!--This is the see product button-->
-									<a href="/detail.php?id=<?php echo $row['product_id'] ?>" class="btn btn-primary float-right">See Product</a>
-								</div>
-							</div>
-						</div>
-						<?php }; ?>
-						<!--Close while loop-->
-				</div>
-   		     
-        
-        
-        </div>
-            </div>
+
         <!-- /.container -->
         <?php }; ?>
         <!--Close the loop	-->
---
+++ inc/inc_category_menu.php
@@ -0,0 +1,52 @@
+
+
+<div class="header-category">
+	<!--<form action="../listings.php" method="get">-->
+	<div class="container">
+		<div class="row justify-content-center">
+			<div>
+				<a class="thumbnail" href="#">
+					<img style="height:60px; transform: rotate(45deg); margin-left: 5px; margin-right: 5px;" class="img-fluid" src="/images/Icons/sstyle.svg" alt="">
+				</a>
+			</div>
+			<div>
+				<a class="thumbnail" href="#">
+					<img style="height:60px; transform: rotate(45deg);margin-left: 15px; margin-right: 15px;" class="img-fluid" src="/images/Icons/tstyle.svg" alt="">
+				</a>
+			</div>
+			<div>
+				<a class="thumbnail" href="#">
+					<img style="height:60px; transform: rotate(45deg);margin-left: 15px; margin-right: 15px;" class="img-fluid" src="/images/Icons/singlecut.svg" alt="">
+				</a>
+			</div>
+			<div class="row">
+				<div>
+					<a class="thumbnail" href="#">
+						<img style="height:60px; transform: rotate(45deg);margin-left: 15px; margin-right: 15px;" class="img-fluid" src="/images/Icons/doublecut.svg" alt="">
+					</a>
+				</div>
+			<div>
+				<a class="thumbnail" href="#">
+					<img style="height:60px; transform: rotate(45deg);margin-left: 15px; margin-right: 15px;" class="img-fluid" src="/images/Icons/offset.svg" alt="">
+				</a>
+			</div>
+			<div>
+				<a class="thumbnail" href="#">
+					<img style="height:60px; transform: rotate(45deg);margin-left: 15px; margin-right: 15px;" class="img-fluid" src="/images/Icons/hollowbody.svg" alt="">
+				</a>
+			</div>
+			<div>
+				<a class="thumbnail" href="#">
+					<img style="height:60px; transform: rotate(45deg);margin-left: 15px; margin-right: 15px;" class="img-fluid" src="/images/Icons/accessories.svg" alt="">
+				</a>
+			</div>
+	<!--
+		<div class="input-group">
+			<div style="height: 30px;"><a href="/detail.php?id=<?php echo $row['product_id'] ?>"><img class="card-img-top" style="width:100%; height:100%;" src="/images/Icons/sstyle.svg" alt=""></a></div>
+							
+		</div>
+		-->
+		</div>
+	</div>
+	<!--</form>-->
+</div>
